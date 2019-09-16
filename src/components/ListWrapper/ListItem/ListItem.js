d93ea913184427c31d34cf70b114a4ea202688b8
Marta <m.olejniczak550@gmail.com>
2019-09-16 20:03:03 +0200

    twitterAcounts links

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './ListItem.css';
-import danAbramovImage from '../../../assets/images/danabramov.jpg';
 
 const ListItem = (props) => (
     <li className="listItem__wrapper">
-        <img src={props.image} className="listItem__image"/>
+        <img 
+            src={props.image} 
+            className="listItem__image"
+            alt={props.name}
+        />
         <div>
             <h2 className="listItem__name"><b>{props.name}</b></h2>
             <p className="listItem__description">{props.description}</p>
-            <button className="listItem__button">Visit Twitter Page</button>
+            <a href={props.link}className="listItem__button">Visit Twitter Page</a>
         </div>
     </li>
 );
