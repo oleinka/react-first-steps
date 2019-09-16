6be7fb91b7c8e0a7ba2ce2ec4d5336e644ed863a
Marta <m.olejniczak550@gmail.com>
2019-09-16 20:19:03 +0200

    array funcion map

--- src/components/ListWrapper/ListWrapper.js
+++ src/components/ListWrapper/ListWrapper.js
@@ -5,30 +5,14 @@ import {twitterAccounts} from '../../data/twitterAccounts';
 
 const ListWrapper = () => (
     <ul className="listWrapper__wrapper">
+        {twitterAccounts.map(item => (
         <ListItem
-            name={twitterAccounts[0].name}
-            description={twitterAccounts[0].description}
-            image={twitterAccounts[0].image}
-            link={twitterAccounts[0].twitterLink}
+            name={item.name}
+            description={item.description}
+            image={item.image}
+            link={item.twitterLink}
         />
-        <ListItem
-            name={twitterAccounts[1].name}
-            description={twitterAccounts[1].description}
-            image={twitterAccounts[1].image}
-            link={twitterAccounts[1].twitterLink}
-        />
-        <ListItem
-            name={twitterAccounts[2].name}
-            description={twitterAccounts[2].description}
-            image={twitterAccounts[2].image}
-            link={twitterAccounts[2].twitterLink}
-        />
-        <ListItem
-            name={twitterAccounts[3].name}
-            description={twitterAccounts[3].description}
-            image={twitterAccounts[3].image}
-            link={twitterAccounts[3].twitterLink}
-        />
+        ))}
     </ul>
 );
 export default ListWrapper;
