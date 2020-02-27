16088511377cd3f5c39c8fae1e26e74b96f830ee
Marta <m.olejniczak550@gmail.com>
2020-02-27 22:01:42 +0100

    initial state array

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -2,16 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './ListItem.css';
 
-const ListItem = ({name, description, image, twitterLink}) => {
-    return (<li className="listItem__wrapper">
-        <img src={image} className="listItem__image" alt={name} />
-        <div>
-            <h2 className="listItem__name"><b>{name}</b></h2>
-            <p className="listItem__description">{description}</p>
-            <a href={twitterLink} className="listItem__button">Visit Twitter Page</a>
-        </div>
-    </li>);
-};
+const ListItem = ({
+    name, 
+    description, 
+    image, 
+    twitterLink
+}) => (
+<li className="listItem__wrapper">
+    <img src={image} className="listItem__image" alt={name} />
+    <div>
+        <h2 className="listItem__name"><b>{name}</b></h2>
+        <p className="listItem__description">{description}</p>
+        <a href={twitterLink} className="listItem__button">Visit Twitter Page</a>
+    </div>
+</li>);
+
 ListItem.propTypes = {
     name: PropTypes.string.isRequired,
     description: PropTypes.string,
