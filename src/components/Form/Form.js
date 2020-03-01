7c03dbfb4d6e3cc5fd8f77f9aaafdb82b8945473
Marta <m.olejniczak550@gmail.com>
2020-03-01 18:38:26 +0100

    components From and Input

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,30 +1,37 @@
 import React from 'react';
 import styles from './Form.module.scss'; 
+import Input from '../Input/Input';
 
 const Form = ({submitFn}) => (
-    <form autoComplete='off' className={styles.wrapper} onSubmit={submitFn}>
-        <h3>Add new Twitter Account</h3>
-        <input 
-            placeholder="name" 
-            name="name"
-            required/>
-        <input 
-            placeholder="link" 
-            name="link"
-            required/>
-        <input 
-            placeholder="image" 
-            name="image"
+    <div className={styles.wrapper}>
+        <h2>Add new Twitter Account</h2>
+        <form autoComplete='off' className={styles.form} onSubmit={submitFn}>
+            <Input
+                name='name'
+                label="Name"
+                maxLength={30}
             />
-        <textarea 
-            placeholder="description" 
-            name="description"
-            required/>
-        <button 
-            type="submit">
-            add new item
-        </button>
-    </form>
+            <Input
+                name='link'
+                label="Twitter Link"
+            />
+            <Input
+                name='image'
+                label="Image"
+            />
+            <Input
+                tag="textarea"
+                name='description'
+                label="Description"
+                maxLength={30}
+            />
+            <button 
+                className={styles.button}
+                type="submit">
+                add new item
+            </button>
+        </form>
+    </div>
 );
 
 export default Form; 
