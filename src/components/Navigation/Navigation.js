dc71096707b792a6c760bc3ea79d929f2e0a320a
Marta <m.olejniczak550@gmail.com>
2020-03-09 20:38:36 +0100

    navigation with link

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => (
     <ul>
-        <li><a href="/">Twitters</a></li>
-        <li><a href="/articles">Articles</a></li>
-        <li><a href="/notes">Notes</a></li>
+        <li><Link to="/">Twitters</Link></li>
+        <li><Link to="/articles">Articles</Link></li>
+        <li><Link to="/notes">Notes</Link></li>
     </ul>
 );
 
